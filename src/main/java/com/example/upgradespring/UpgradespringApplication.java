3e0c17883b4d5faa19333b6f07967d0485b78053
Victor Bahta Teka <78victors@gmail.com>
2024-03-31 20:54:50 -0500

    initial commit

---
+++ src/main/java/com/example/upgradespring/UpgradespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.upgradespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradespringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/upgradespring/UpgradespringApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.example.upgradespring;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class UpgradespringApplicationTests {
+
+// @Test
+// void contextLoads() {
+// }
+
+// }
